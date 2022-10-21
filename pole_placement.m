24aee949ab2e5e5b3fc8834efe539a1978c40417
cyh <e1010473@u.nus.edu>
2022-10-21 22:22:17 +0800

    update

--- pole_placement.m
+++ pole_placement.m
@@ -3,23 +3,29 @@
 %   w_n = 1.13
 %   H_d(s) = (1.2769) / (s^2 + 1.59782*s + 1.2769)
 %   lamda12 = -zita*w_n +- j w_n*sqrt(1-zita*zita);
-function [K] = pole_placement(A, B)
+function [K11,K] = pole_placement(A, B)
 
 syms s;
 damp = 0.707;
+% damp = 0.595;
 wn = 1.13;
 lamda1 = -damp*wn + wn*sqrt(1-damp*damp)*1i;
 lamda2 = -damp*wn - wn*sqrt(1-damp*damp)*1i;
 %   2 times faster
-% lamda3 = -1.6;
-lamda3 = -2.4 + wn*sqrt(1-damp*damp)*1i;
-lamda4 = -2.4 - wn*sqrt(1-damp*damp)*1i;
+lamda3 = -1.6;
+lamda4 = -2.4;
+% lamda3 = -2.4 + wn*sqrt(1-damp*damp)*1i;
+% lamda4 = -2.4 - wn*sqrt(1-damp*damp)*1i;
 %   4 times faster
-% lamda5 = -3.2;
-lamda5 = -4.0 + wn*sqrt(1-damp*damp)*1i;
-lamda6 = -4.0 - wn*sqrt(1-damp*damp)*1i;
+lamda5 = -3.2;
+lamda6 = -4.0;
+% lamda5 = -4.0 + wn*sqrt(1-damp*damp)*1i;
+% lamda6 = -4.0 - wn*sqrt(1-damp*damp)*1i;
 polynomial = (s-lamda1)*(s-lamda2)*(s-lamda3)*(s-lamda4)*(s-lamda5)*(s-lamda6);
 pol_cof = double(coeffs(polynomial));
+
+p = [lamda1 lamda2, lamda3 lamda4 lamda5 lamda6];
+K11 = place(A,B,p);
 
 Ad = [0   1   0   0   0   0;
       0   0   1   0   0   0;
@@ -46,11 +52,15 @@ T = [C_inv(d1,:);
      C_inv(d1+d2,:)*A*A];
 Aba = T*A/T;
 Bba = T*B;
-Aba(abs(Aba) < 1e-10) = 0;
-Bba(abs(Bba) < 1e-10) = 0;
+% Aba = round(Aba);
+% Bba = round(Bba);
+Aba(abs(Aba) < 1e-5) = 0;
+Bba(abs(Bba) < 1e-5) = 0;
 
-equation = Ad == Aba-Bba*K;
-K_num = solve(equation);
+K_num = solve(Ad([3,6],:) == Aba([3,6],:) - Bba([3,6],:)*K);
+
+
+% K_num = solve(Ad == Aba-Bba*K);
 K_ans = double(struct2array(K_num));
 Kba = [K_ans(1:6);
        K_ans(7:12)];
